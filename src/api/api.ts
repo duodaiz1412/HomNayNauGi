30a77a2d5f8d4589585bf9e935b467f9befd9421
Duong <loduong4992@gmail.com>
2025-05-17 09:43:31 +0700

    fix: remove post management

--- src/api/api.ts
+++ src/api/api.ts
@@ -6,7 +6,7 @@ import * as ImageManipulator from 'expo-image-manipulator';
 import { IngredientSearch } from '../types';
 
 const api = axios.create({
-  baseURL: 'http://192.168.100.87:3001',
+  baseURL: 'http://192.168.1.195:3001',
   // timeout: 50000,
   // headers: {
   //   Accept: 'application/json',
